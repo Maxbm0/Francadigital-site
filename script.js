b6292bd0e419dd4688bb90f1946d87f1dceedb99
Maxbm0 <maxbmansano.z@gmail.com>
2025-11-22 11:49:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const seletorCor = document.getElementById("seletorCor");
+const btnAplicarCor = document.getElementById("btnAplicarCor")
+
+btnAplicarCor.addEventListener("click", (e) => {
+    const cor = seletorCor.value;
+    document.documentElement.style.setProperty("--cor-fundo", cor);
+});
+
+const chips = document.querySelectorAll(".chip")
+const galeria = document.getElementById("galeriaHero");
+
+const imageminicial = chips[0].dataset.imagem;
+galeria.style.backgroundImage = `url(${imageminicial})`
+
+chips.forEach((chip) => {
+    chip.addEventListener("click", () => {
+        galeria.classList.add("ativa");
+        galeria.style.backgroundImage = `url(${chip.dataset.image})`;
+    });
+});
